dbdc260b03b35036f86e1f50df441bb54cbd6c7c
amo3223 <50189394+amo3223@users.noreply.github.com>
2020-05-18 14:14:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,29 +5,29 @@ var app = new Vue({
         itemList:[
           {
             id:'1',
-            itemName:'優質短袖白T',
+            itemName:'漢堡',
             imgUrl:"https://i.imgur.com/Pnz3eEH.jpg",
             price:'500',
             count:'2'
           },
           {
             id:'2',
-            itemName:'骷髏手短黑T',
-            imgUrl:'https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+            itemName:'牛肉麵',
+            imgUrl:'https://i.imgur.com/MxLHdZC.jpg',
             price:'790',
             count:'3'
           },
           {
             id:'3',
-            itemName:'超時尚牛仔庫',
-            imgUrl:'https://images.unsplash.com/photo-1529391409740-59f2cea08bc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1124&q=80',
+            itemName:'redbull',
+            imgUrl:'https://i.imgur.com/JGslzHw.jpg',
             price:'1200',
             count:'1'
           },
           {
             id:'4',
-            itemName:'質感褐色系大衣服',
-            imgUrl:'https://images.unsplash.com/photo-1491998664548-0063bef7856c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+            itemName:'足球',
+            imgUrl:'https://i.imgur.com/X8992RE.jpg',
             price:'2300',
             count:'1'
           },
